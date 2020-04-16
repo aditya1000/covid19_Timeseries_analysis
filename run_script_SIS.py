963bc2eead2a3164c2b160f63226e5a546ae6aac
amogh <amogh7777@gmail.com>
2020-04-16 20:14:22 +0530

    SIS model error

--- run_script_SIS.py
+++ run_script_SIS.py
@@ -1,8 +1,9 @@
 import os
 PATH = "/mnt/c/Users/amogh/Documents/r_scripts"
-args = ["Delhi","100","10","T","0.001","0.002","0.001","0.002"]
+args = ["Delhi","100","10","0.002","0.002"]
 out = " "
 for a in args:
     out = out + a + " "
 # print(out)
+print("Rscript SIS_projections_simulate.R " + out)
 os.system("Rscript SIS_projections_simulate.R " + out)
